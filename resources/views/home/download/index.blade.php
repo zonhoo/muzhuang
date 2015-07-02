0209c2953e5f44f01d3f3b80b5f71ce8dc68a3b5
nutsdo <ldingtao@163.com>
2015-07-02 15:05:32 +0800

    修改下载页面

--- resources/views/home/download/index.blade.php
+++ resources/views/home/download/index.blade.php
@@ -13,22 +13,17 @@
   <head>
 
     <meta charset="utf-8">
-    <title>Bootstrap中文网</title>
+    <title>慕庄</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Bootstrap是Twitter推出的一个用于前端开发的开源工具包。它由Twitter的设计师Mark Otto和Jacob Thornton合作开发，是一个CSS/HTML框架。目前，Bootstrap最新版本为3.0 。Bootstrap中文网致力于为广大国内开发者提供详尽的中文文档、代码实例等，助力开发者掌握并使用这一框架。">
-    <meta name="keywords" content="Bootstrap,CSS,CSS框架,CSS framework,javascript,bootcss,bootstrap开发,bootstrap代码,bootstrap入门">
-    <meta name="author" content="Bootstrap中文网">
-    <meta name="robots" content="index,follow">
-    <meta name="application-name" content="bootcss.com">
-    <meta property="qc:admins" content="1603466166416277433363757477167" />
-    <style>
-    body{padding-top:0!important; }
-    </style>
+    <meta name="description" content="庄里人的城市生活指南">
+    <meta name="keywords" content="庄里人的城市生活指南">
+    <meta name="author" content="石家庄荣鹏科技有限公司">
     <!-- Site CSS -->
     <link href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
     <link href="http://cdn.bootcss.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="http://static.bootcss.com/www/assets/css/site.min.css?v5" rel="stylesheet">
     <style>
+    body{padding-top:0!important; }
     .job-hot {
     	position: absolute;
     	color: #d9534f;
@@ -37,26 +32,6 @@
     }
     </style>
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="/assets/js/html5shiv.min.js?v=f3008b4099"></script>
-      <script src="/assets/js/respond.min.js?v=f3008b4099"></script>
-    <![endif]-->
-
-    <!-- Favicons -->
-    <link rel="apple-touch-icon-precomposed" href="http://static.bootcss.com/www/assets/ico/apple-touch-icon-precomposed.png">
-    <link rel="shortcut icon" href="http://static.bootcss.com/www/assets/ico/favicon.png">
-
-    <script>
-      var _hmt = _hmt || [];
-    </script>
-
-    <link rel="canonical" href="http://www.bootcss.com/" />
-    <meta name="generator" content="Ghost 0.5" />
-    <link rel="alternate" type="application/rss+xml" title="Bootstrap中文网" href="http://www.bootcss.com/rss/" />
-    <script>
-var qqgroup = '136374923';
-</script>
 
   </head>
   <body class="home-template">
@@ -109,35 +84,6 @@ var qqgroup = '136374923';
         </div>
       </div>
     </footer>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://cdn.bootcss.com/jquery/1.11.2/jquery.min.js"></script>
-    <script src="http://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <script src="http://cdn.bootcss.com/unveil/1.3.0/jquery.unveil.min.js"></script>
-    <script src="http://cdn.bootcss.com/scrollup/2.4.0/jquery.scrollUp.min.js"></script>
-    <script src="http://cdn.bootcss.com/toc/0.3.2/toc.min.js"></script>
-    <script src="http://cdn.bootcss.com/jquery.matchHeight/0.5.2/jquery.matchHeight-min.js"></script>
-    <script src="http://static.bootcss.com/www/assets/js/site.min.js"></script>
-    <script>
-$(document).ready(function(){
-    $('#qqgroup').text(qqgroup);
-});
-</script>
-
-
-    <!-- 以下是 www.bootcss.com 网站所使用的统计代码，如果你使用本页面作为自己的模板，请将下面的统计代码删掉！！！ -->
-        <script type="text/javascript">
-        var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-        document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F3d8e7fc0de8a2a75f2ca3bfe128e6391' type='text/javascript'%3E%3C/script%3E"));
-        </script>
-
-        <script type="text/javascript">
-    var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-    document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F079fac161efc4b2a6f31e80064f14e82' type='text/javascript'%3E%3C/script%3E"));
-    </script>
-
-    <!-- 上面是 www.bootcss.com 网站所使用的统计代码，如果你使用本页面作为自己的模板，请将上面面的统计代码删掉！！！ -->  </body>
+</body>
 </html>
 <!-- static home page -->
